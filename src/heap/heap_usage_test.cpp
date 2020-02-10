672d3ae0db20727878125ad12ddbfcd6e43d1f87
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-11 01:33:04 +0900

    Add heapUsage module

---
+++ src/heap/heap_usage_test.cpp
@@ -0,0 +1,79 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "heap_usage.h"
+#include "heap_usage_internal.h"
+}
+
+namespace {
+bool was_run;
+int given_usage;
+void WarningSpy(int usage) {
+  was_run = true;
+  given_usage = usage;
+}
+}  // namespace
+
+class HeapUsageTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    was_run = false;
+    given_usage = 0;
+    heapUsage->Clear();
+    heapUsage->SetWarning(256, WarningSpy);
+  }
+  virtual void TearDown() {}
+};
+
+TEST_F(HeapUsageTest, WarnWhenNotOverUsageLimit) {
+  heapUsage_->Add(255);
+  heapUsage_->WarnIfNeeded();
+
+  EXPECT_FALSE(was_run);
+}
+
+TEST_F(HeapUsageTest, WarnWhenOverUsageLimit) {
+  heapUsage_->Add(256);
+  heapUsage_->WarnIfNeeded();
+
+  EXPECT_TRUE(was_run);
+  EXPECT_EQ(256, given_usage);
+}
+
+TEST_F(HeapUsageTest, SetWarningWithNull) {
+  heapUsage->SetWarning(255, NULL);
+
+  heapUsage_->Add(256);
+
+  EXPECT_FALSE(was_run);
+}
+
+TEST_F(HeapUsageTest, Clear) {
+  heapUsage_->Add(255);
+
+  heapUsage->Clear();
+  heapUsage_->Add(1);
+
+  EXPECT_FALSE(was_run);
+}
+
+TEST_F(HeapUsageTest, SampleTransaction) {
+  heapUsage_->Add(192);
+  heapUsage_->Subtract(192);
+  heapUsage_->Add(100);
+  heapUsage_->WarnIfNeeded();
+  EXPECT_FALSE(was_run);
+
+  heapUsage_->Add(32);
+  heapUsage_->Subtract(32);
+  heapUsage_->Add(128);
+  heapUsage_->WarnIfNeeded();
+  EXPECT_FALSE(was_run);
+
+  heapUsage_->Add(64);
+  heapUsage_->WarnIfNeeded();
+  EXPECT_TRUE(was_run);
+  EXPECT_EQ(292, given_usage);
+}
---
+++ v1/heap_usage.h
@@ -0,0 +1,15 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_HEAP_USAGE_H_
+#define V1_HEAP_USAGE_H_
+
+typedef void (*WarningDelegate)(int usage);
+typedef struct {
+  void (*Clear)(void);
+  void (*SetWarning)(int usage, WarningDelegate warning);
+} HeapUsageMethodStruct;
+typedef const HeapUsageMethodStruct* HeapUsageMethod;
+
+extern const HeapUsageMethod heapUsage;
+
+#endif  // V1_HEAP_USAGE_H_
