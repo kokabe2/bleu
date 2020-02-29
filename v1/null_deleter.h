aa874553e50c45295483d82e70085dc94f202425
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-29 15:36:36 +0900

    Add NullDeleter class

---
+++ v1/null_deleter.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_NULL_DELETER_H_
+#define V1_NULL_DELETER_H_
+
+#include "deleter_interface.h"
+
+extern const DeleterInterface NullDeleter;
+
+#endif  // V1_NULL_DELETER_H_
