88c7452ec4d033ffa56842b7525d1565a1d2e5db
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-29 15:35:39 +0900

    Add NullComparer class

---
+++ src/util/null_comparer.c
@@ -0,0 +1,11 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "null_comparer.h"
+
+static int Compare(const void* x, const void* y) { return 0; }
+
+static const ComparerInterfaceStruct kTheInterface = {
+    .Compare = Compare,
+};
+
+const ComparerInterface NullComparer = &kTheInterface;
---
+++ v1/null_comparer.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_NULL_COMPARER_H_
+#define V1_NULL_COMPARER_H_
+
+#include "comparer_interface.h"
+
+extern const ComparerInterface NullComparer;
+
+#endif  // V1_NULL_COMPARER_H_
