613fec6734d8460dc73851ffabf81e932602461b
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 01:19:27 +0900

    Add Strings class

---
+++ src/string/str.c
@@ -0,0 +1,57 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "str.h"
+
+#include <string.h>
+
+#include "heap.h"
+
+static Strings New(Strings s) {
+  Strings self = (Strings)heap->New(strlen(s) + 1);
+  return strncpy((char *)self, s, strlen(s));
+}
+
+static void Delete(Strings *self) { heap->Delete((void **)self); }
+
+static int Length(Strings self) { return strlen(self); }
+
+static bool Equals(Strings self, Strings s) { return strncmp(self, s, strlen(self)) == 0; }
+
+static bool Contains(Strings self, Strings s) { return strstr(self, s) != NULL; }
+
+static int Index(Strings self, Strings s) {
+  char *p = strstr(self, s);
+  return p != NULL ? p - self : -1;
+}
+
+static int LastIndex(Strings self, Strings s) {
+  int index = 0;
+  for (int i = Index(self, s); i != -1; index += i + 1, i = Index(&self[index], s)) continue;
+  return index - 1;
+}
+
+static int Count(Strings self, Strings s) {
+  int index = 0;
+  int count = 0;
+  for (int i = Index(self, s); i != -1; index += i + 1, i = Index(&self[index], s), ++count) continue;
+  return count;
+}
+
+static bool HasPrefix(Strings self, Strings s) { return strstr(self, s) == self; }
+
+static bool HasSuffix(Strings self, Strings s) { return strncmp(&self[strlen(self) - strlen(s)], s, strlen(s)) == 0; }
+
+static const StringsMethodStruct kTheMethod = {
+    .New = New,
+    .Delete = Delete,
+    .Length = Length,
+    .Equals = Equals,
+    .Contains = Contains,
+    .Index = Index,
+    .LastIndex = LastIndex,
+    .Count = Count,
+    .HasPrefix = HasPrefix,
+    .HasSuffix = HasSuffix,
+};
+
+const StringsMethod strings = &kTheMethod;
---
+++ v1/str.h
@@ -0,0 +1,25 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_STR_H_
+#define V1_STR_H_
+
+#include <stdbool.h>
+
+typedef const char* Strings;
+typedef struct {
+  Strings (*New)(Strings s);
+  void (*Delete)(Strings* self);
+  int (*Length)(Strings self);
+  bool (*Equals)(Strings self, Strings s);
+  bool (*Contains)(Strings self, Strings s);
+  int (*Index)(Strings self, Strings s);
+  int (*LastIndex)(Strings self, Strings s);
+  int (*Count)(Strings self, Strings s);
+  bool (*HasPrefix)(Strings self, Strings s);
+  bool (*HasSuffix)(Strings self, Strings s);
+} StringsMethodStruct;
+typedef const StringsMethodStruct* StringsMethod;
+
+extern const StringsMethod strings;
+
+#endif  // V1_STR_H_
