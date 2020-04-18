f36e0af4fd1c6e5d74f1ccae18e55eeaff311be3
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-04-18 21:09:50 +0900

    Use delegate_definition.h

--- v1/list.h
+++ v1/list.h
@@ -3,10 +3,8 @@
 #ifndef V1_LIST_H_
 #define V1_LIST_H_
 
-typedef int (*CompareDelegate)(
-    const void* x,
-    const void* y);  // Returns less than 0 if x is less than y, 0 if x equals y, greater than 0 if x is greater than y.
-typedef void (*DeleteDelegate)(void** x);  // x shall be null if deleted successfully.
+#include "delegate_definition.h"
+
 typedef struct ListStruct* List;
 typedef struct {
   List (*New)(void);
